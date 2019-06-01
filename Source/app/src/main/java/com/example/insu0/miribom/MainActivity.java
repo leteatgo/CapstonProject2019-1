5dee11a7f0fd435767a934a05807df656b3a5dcf
양인수 <insu0929@naver.com>
2019-06-01 15:28:45 +0900

    네이버 로그인 모듈 추가

--- Source/app/src/main/java/com/example/insu0/miribom/MainActivity.java
+++ Source/app/src/main/java/com/example/insu0/miribom/MainActivity.java
@@ -1,23 +1,38 @@
 package com.example.insu0.miribom;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Paint;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.insu0.miribom.Servers.MiribomInfo;
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     Button main_kakaoBtn;
-    Button main_naverBtn;
     Button main_emailBtn;
     TextView main_joinText;
 
@@ -26,6 +41,19 @@ public class MainActivity extends AppCompatActivity {
     * @author ckddn
     * added 190412
     * */
+
+    /*implemented by Insu Yang*/
+
+    /*Naver Login Module*/
+    private OAuthLoginButton main_naverBtn;
+    private Map<String,String> myUserInfoMap; //여기에 네이버 유저 정보 담김
+
+    private static String CLIENT_ID = "aePp5k4iytz4eAsaenxW";
+    private static String CLIENT_SECRET = "fJfOxprZL6";
+    private static String CLIENT_NAME = "미리봄";
+    private static OAuthLogin mOAuthLoginInstance;
+    private static Context mContext;
+
     @Override
     protected void onStart() {
 
@@ -49,6 +77,10 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        mContext = this;
+        initNaver();
+
+
         main_joinText = findViewById(R.id.main_jointext);
         main_joinText.setOnClickListener(new Button.OnClickListener() {
             @Override
@@ -66,6 +98,176 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        main_naverBtn = (OAuthLoginButton) findViewById(R.id.main_naverBtn);
+        main_naverBtn.setOAuthLoginHandler(mOAuthLoginHandler);
+        main_naverBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mOAuthLoginInstance.startOauthLoginActivity( MainActivity.this, mOAuthLoginHandler);
+
+            }
+        });
+
+
+
+    }
+
+    private void initNaver(){
+        //Naver Instance Initialization
+        mOAuthLoginInstance = OAuthLogin.getInstance();
+        mOAuthLoginInstance.showDevelopersLog(true);
+        mOAuthLoginInstance.init(mContext,CLIENT_ID,CLIENT_SECRET,CLIENT_NAME);
+    }
+
+    @Override
+    protected void onResume() {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        super.onResume();
+
+    }
+
+    private OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+        @Override
+        public void run(boolean success) {
+            if (success) {
+                String accessToken = mOAuthLoginInstance.getAccessToken(mContext);
+                String refreshToken = mOAuthLoginInstance.getRefreshToken(mContext);
+                long expiresAt = mOAuthLoginInstance.getExpiresAt(mContext);
+                String tokenType = mOAuthLoginInstance.getTokenType(mContext);
+                Log.d(TAG,accessToken);
+                Log.d(TAG,refreshToken);
+                Log.d(TAG,String.valueOf(expiresAt));
+                Log.d(TAG,tokenType);
+                Log.d(TAG, mOAuthLoginInstance.getState(mContext).toString());
+                new RequestApiTask().execute();
+            } else {
+                String errorCode = mOAuthLoginInstance.getLastErrorCode(mContext).getCode();
+                String errorDesc = mOAuthLoginInstance.getLastErrorDesc(mContext);
+                Toast.makeText(mContext, "errorCode:" + errorCode + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+                Log.d(TAG,"errorDesc:"+ errorDesc);
+            }
+        };
+    };
+
+    private class DeleteTokenTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            boolean isSuccessDeleteToken = mOAuthLoginInstance.logoutAndDeleteToken(mContext);
+
+            if (!isSuccessDeleteToken) {
+                // 서버에서 token 삭제에 실패했어도 클라이언트에 있는 token 은 삭제되어 로그아웃된 상태이다
+                // 실패했어도 클라이언트 상에 token 정보가 없기 때문에 추가적으로 해줄 수 있는 것은 없음
+                Log.d(TAG, "errorCode:" + mOAuthLoginInstance.getLastErrorCode(mContext));
+                Log.d(TAG, "errorDesc:" + mOAuthLoginInstance.getLastErrorDesc(mContext));
+            }
+
+            return null;
+        }
+    }
+
+    private class RequestApiTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            String url = "https://openapi.naver.com/v1/nid/getUserProfile.xml";
+            String at = mOAuthLoginInstance.getAccessToken(mContext);
+            myUserInfoMap = requestNaverUserInfo(mOAuthLoginInstance.requestApi(mContext, at, url));
+            return null;
+        }
+
+        protected void onPostExecute(Void content) {
+            if (myUserInfoMap.get("email") == null) {
+                Toast.makeText(mContext, "로그인 실패하였습니다.  잠시후 다시 시도해 주세요!!", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.d(TAG, String.valueOf(myUserInfoMap));
+                Toast.makeText(mContext, myUserInfoMap.get("enc_id") + "님 환영합니다", Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+
+        private Map<String, String> requestNaverUserInfo(String data) { // xml 파싱
+            String f_array[] = new String[9];
+
+            try {
+                XmlPullParserFactory parserCreator = XmlPullParserFactory
+                        .newInstance();
+                XmlPullParser parser = parserCreator.newPullParser();
+                InputStream input = new ByteArrayInputStream(
+                        data.getBytes("UTF-8"));
+                parser.setInput(input, "UTF-8");
+
+                int parserEvent = parser.getEventType();
+                String tag;
+                boolean inText = false;
+                boolean lastMatTag = false;
+
+                int colIdx = 0;
+
+                while (parserEvent != XmlPullParser.END_DOCUMENT) {
+                    switch (parserEvent) {
+                        case XmlPullParser.START_TAG:
+                            tag = parser.getName();
+                            if (tag.compareTo("xml") == 0) {
+                                inText = false;
+                            } else if (tag.compareTo("data") == 0) {
+                                inText = false;
+                            } else if (tag.compareTo("result") == 0) {
+                                inText = false;
+                            } else if (tag.compareTo("resultcode") == 0) {
+                                inText = false;
+                            } else if (tag.compareTo("message") == 0) {
+                                inText = false;
+                            } else if (tag.compareTo("response") == 0) {
+                                inText = false;
+                            } else {
+                                inText = true;
+
+                            }
+                            break;
+                        case XmlPullParser.TEXT:
+                            tag = parser.getName();
+                            if (inText) {
+                                if (parser.getText() == null) {
+                                    f_array[colIdx] = "";
+                                } else {
+                                    f_array[colIdx] = parser.getText().trim();
+                                }
+
+                                colIdx++;
+                            }
+                            inText = false;
+                            break;
+                        case XmlPullParser.END_TAG:
+                            tag = parser.getName();
+                            inText = false;
+                            break;
+
+                    }
+
+                    parserEvent = parser.next();
+                }
+            } catch (Exception e) {
+                Log.e("dd", "Error in network call", e);
+            }
+            Map<String, String> resultMap = new HashMap<>();
+            resultMap.put("email", f_array[0]);
+            resultMap.put("nickname", f_array[1]);
+            resultMap.put("enc_id", f_array[2]);
+            resultMap.put("profile_image", f_array[3]);
+            resultMap.put("age", f_array[4]);
+            resultMap.put("gender", f_array[5]);
+            resultMap.put("id", f_array[6]);
+            resultMap.put("name", f_array[7]);
+            resultMap.put("birthday", f_array[8]);
+            return resultMap;
+        }
 
     }
 }
