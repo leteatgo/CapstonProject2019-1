7ad9daa2d9385a0b1bb4af196b9abb2b8ccc5858
손창우 <ckddn9496@gmail.com>
2019-06-01 18:48:13 +0900

    Combine Modules

--- Source/app/src/main/java/com/example/insu0/miribom/MainActivity.java
+++ Source/app/src/main/java/com/example/insu0/miribom/MainActivity.java
@@ -21,11 +21,22 @@ import com.nhn.android.naverlogin.OAuthLogin;
 import com.nhn.android.naverlogin.OAuthLoginHandler;
 import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.mindrot.jbcrypt.BCrypt;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,7 +49,8 @@ public class MainActivity extends AppCompatActivity {
 
     /*Naver Login Module*/
     private OAuthLoginButton main_naverBtn;
-    private Map<String,String> myUserInfoMap; //여기에 네이버 유저 정보 담김
+    private JSONObject naverUserAccountInfo;
+//    private Map<String,String> myUserInfoMap; //여기에 네이버 유저 정보 담김
 
     private static String CLIENT_ID = "aePp5k4iytz4eAsaenxW";
     private static String CLIENT_SECRET = "fJfOxprZL6";
@@ -168,24 +180,29 @@ public class MainActivity extends AppCompatActivity {
             String url = "https://openapi.naver.com/v1/nid/getUserProfile.xml";
             String at = mOAuthLoginInstance.getAccessToken(mContext);
             String data = mOAuthLoginInstance.requestApi(mContext, at, url);
-            myUserInfoMap = requestNaverUserInfo(mOAuthLoginInstance.requestApi(mContext, at, url));
+            naverUserAccountInfo = requestNaverUserInfo(data);
+//            myUserInfoMap = requestNaverUserInfo(data);
             return data;
         }
 
         protected void onPostExecute(String data) {
-            Toast.makeText(mContext, data, Toast.LENGTH_LONG).show();
-            if (myUserInfoMap.get("email") == null) {
-                Toast.makeText(mContext, "로그인 실패하였습니다.  잠시후 다시 시도해 주세요!!", Toast.LENGTH_SHORT).show();
-            } else {
-                Log.d(TAG, String.valueOf(myUserInfoMap));
-
+            try {
+                if (naverUserAccountInfo.getInt("resultcode") == 00) {
+                    Toast.makeText(mContext, "안녕하세요 " + naverUserAccountInfo.get("name")+ "님!", Toast.LENGTH_LONG).show();
+                    //  send server to login
+                    new SignInByNaverTask().execute("http://" + MiribomInfo.ipAddress + "/login/signIn/nhn");
+                }
+                else
+                    Toast.makeText(mContext, "로그인에 실패하였습니다.", Toast.LENGTH_LONG).show();
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-
         }
-
-        private Map<String, String> requestNaverUserInfo(String data) { // xml 파싱
+        private JSONObject requestNaverUserInfo(String data) { // xml 파싱
+//        private Map<String, String> requestNaverUserInfo(String data) { // xml 파싱
             String f_array[] = new String[9];
-
+//            Map<String, String> resultMap = new HashMap<>();
+            naverUserAccountInfo = new JSONObject();
             try {
                 XmlPullParserFactory parserCreator = XmlPullParserFactory
                         .newInstance();
@@ -195,11 +212,9 @@ public class MainActivity extends AppCompatActivity {
                 parser.setInput(input, "UTF-8");
 
                 int parserEvent = parser.getEventType();
-                String tag;
+                String tag = "";
                 boolean inText = false;
                 boolean lastMatTag = false;
-
-                int colIdx = 0;
 
                 while (parserEvent != XmlPullParser.END_DOCUMENT) {
                     switch (parserEvent) {
@@ -211,10 +226,6 @@ public class MainActivity extends AppCompatActivity {
                                 inText = false;
                             } else if (tag.compareTo("result") == 0) {
                                 inText = false;
-                            } else if (tag.compareTo("resultcode") == 0) {
-                                inText = false;
-                            } else if (tag.compareTo("message") == 0) {
-                                inText = false;
                             } else if (tag.compareTo("response") == 0) {
                                 inText = false;
                             } else {
@@ -223,15 +234,16 @@ public class MainActivity extends AppCompatActivity {
                             }
                             break;
                         case XmlPullParser.TEXT:
-                            tag = parser.getName();
-                            if (inText) {
+//                            Log.d(TAG, "requestNaverUserInfo: tag name: " + tag);
+                            if (inText) {   //  true 받을 값 이면
                                 if (parser.getText() == null) {
-                                    f_array[colIdx] = "";
+//                                    resultMap.put(tag, "");
+                                    naverUserAccountInfo.put(tag,"");
                                 } else {
-                                    f_array[colIdx] = parser.getText().trim();
+//                                    resultMap.put(tag,parser.getText().trim());
+                                    naverUserAccountInfo.put(tag, parser.getText().trim());
                                 }
-
-                                colIdx++;
+//                                Log.d(TAG, "requestNaverUserInfo: text: "+ parser.getText());
                             }
                             inText = false;
                             break;
@@ -239,7 +251,6 @@ public class MainActivity extends AppCompatActivity {
                             tag = parser.getName();
                             inText = false;
                             break;
-
                     }
 
                     parserEvent = parser.next();
@@ -247,19 +258,91 @@ public class MainActivity extends AppCompatActivity {
             } catch (Exception e) {
                 Log.e("dd", "Error in network call", e);
             }
-            Map<String, String> resultMap = new HashMap<>();
-            resultMap.put("email", f_array[0]);
-            resultMap.put("nickname", f_array[1]);
-            resultMap.put("enc_id", f_array[2]);
-            resultMap.put("profile_image", f_array[3]);
-            resultMap.put("age", f_array[4]);
-            resultMap.put("gender", f_array[5]);
-            resultMap.put("id", f_array[6]);
-            resultMap.put("name", f_array[7]);
-            resultMap.put("birthday", f_array[8]);
-            return resultMap;
+
+            return naverUserAccountInfo;
         }
 
     }
 
+
+    public class SignInByNaverTask extends AsyncTask<String, String, String> {
+        String TAG = "SignInByNaverTask>>>";
+
+        @Override
+        protected String doInBackground(String... strings) {    //  id로 서버에서 salt hash 가져옴
+            try {
+                JSONObject loginInfo = new JSONObject();
+                loginInfo.accumulate("id", naverUserAccountInfo.getString("email"));
+                String salt = BCrypt.gensalt();
+                String hash = BCrypt.hashpw(naverUserAccountInfo.getString("id"), salt);
+                loginInfo.accumulate("hash", hash);
+                loginInfo.accumulate("salt", salt);
+                loginInfo.accumulate("name", naverUserAccountInfo.getString("name"));
+
+                HttpURLConnection conn = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL(strings[0]);
+                    // settings
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Cache-Control", "no-cache");
+                    conn.setRequestProperty("Content-Type", "application/json");
+                    conn.setRequestProperty("Accept", "application/text");
+                    conn.setRequestProperty("Accept", "application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+                    conn.connect();
+
+                    OutputStream outputStream = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+                    writer.write(loginInfo.toString());
+                    writer.flush();
+                    writer.close();
+
+                    InputStream stream = conn.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuffer buffer = new StringBuffer();
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line);
+                        Log.d(TAG, "doInBackground: readLine, " + line);
+                    }
+                    return buffer.toString();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(TAG, "onPostExecute: " + result);
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                intent.putExtra("no", jsonObject.getInt("no"));
+                intent.putExtra("id", naverUserAccountInfo.getString("email"));
+                Log.d(TAG, "onPostExecute: json");
+                startActivity(intent);
+            } catch (JSONException e) {
+                int no = Integer.parseInt(result.substring(1, result.length() - 1));
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                intent.putExtra("no", no);
+                try {
+                    intent.putExtra("id", naverUserAccountInfo.getString("email"));
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
+                }
+                Log.d(TAG, "onPostExecute: catch");
+                startActivity(intent);
+            }
+        }
+    }
 }
